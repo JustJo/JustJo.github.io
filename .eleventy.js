7a7a4a4d5a1890d88ceac11bf0143111869a6f21
Jo Walter <joanne.walter87@gmail.com>
2021-08-15 13:45:51 +1000

    Tic Tac Toe Prototype

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function(eleventyConfig) {
   // Return your Object options:
   eleventyConfig.addPassthroughCopy("resources");
+  eleventyConfig.setTemplateFormats([
+  "hbs",
+  "html",
+  "css" // css is not yet a recognized template extension in Eleventy
+  ]);
   return {
     dir: {
       output: "docs"
