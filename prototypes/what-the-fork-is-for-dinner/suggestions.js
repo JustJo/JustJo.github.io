962c1b80263574664bef8a1909a9865095cebaba
Jo Walter <joanne.walter87@gmail.com>
2024-12-15 17:27:16 +1000

    what the fork is for dinner initial setup

---
+++ prototypes/what-the-fork-is-for-dinner/suggestions.js
@@ -0,0 +1,64 @@
+var standardOptions = [
+  ["Japanese curry", "", ""],
+  ["Cottage pie", "", ""],
+  ["Spaghetti bolognese", "", ""],
+  ["Steak","with vegetables", ""],
+  ["Lamb steak", "with vegetables", ""],
+  ["Sausages","with mash and vegetables", ""],
+  ["German sausage", "with sauerkraut, mash and vegetables", ""],
+  ["Cacio e pepe", "", ""],
+  ["Pizza", "", ""],
+  ["Salmon", "with vegetables",""],
+  ["Hot pot","",""],
+  ["Pumpkin soup", "with garlic bread",""],
+  ["Chicken pasta","","https://www.tamingtwins.com/marry-me-chicken-pasta/"],
+  ["Mexican chicken and rice","","https://www.tamingtwins.com/marry-me-chicken-pasta/"],
+  ["Beef goulash","","https://www.tamingtwins.com/beef-goulash/"],
+  ["Sticky beef & noodles","","https://www.tamingtwins.com/beef-noodles/"],
+  ["Chicken quesadillas","","https://www.tamingtwins.com/chicken-quesadillas/"],
+  ["Halloumi pasta","","https://www.tamingtwins.com/halloumi-pasta/"],
+  ["Peri peri chicken pasta","","https://www.tamingtwins.com/peri-peri-chicken-pasta/"],
+  ["Red lentil dahl","","https://www.tamingtwins.com/slow-cooker-dahl/"],
+  ["Sweet potato & peanut butter stew","","https://www.tamingtwins.com/sweet-potato-peanut-vegan-stew/"],
+  ["Lamb tagine","with couscous","https://www.tamingtwins.com/lamb-tagine-recipe/"],
+  ["Honey mustard chicken","","https://www.tamingtwins.com/honey-mustard-chicken-traybake/"],
+  ["Spinach and cheese pasta bake","","https://simplehomeedit.com/recipe/spinach-and-cheese-pasta-bake/"],
+  ["Chicken burrito bowls","","https://simplehomeedit.com/recipe/chicken-burrito-bowls/"],
+  ["Spring roll bowls","","https://simplehomeedit.com/recipe/spring-roll-bowls/"],
+  ["Beef teriyaki","","https://simplehomeedit.com/recipe/speedy-beef-teriyaki/"],
+  ["Pesto chicken pasta","","https://simplehomeedit.com/recipe/one-pan-pesto-chicken-pasta/"],
+  ["Chili Con Carne","",""],
+  ["Satay chicken","with rice or noodles",""],
+  ["Stroganoff","with pasta or egg noodles",""],
+  ["Buttery chicken","with rice and vegetables",""]
+];
+
+var takeawayOptions = [
+  ["Yiros", "The Yiros Shop", "https://theyirosshop.com.au/our-menu/"],
+  ["Thai ", "Bann Phaya", "https://www.baanphayathaidakabin.com.au/"],
+];
+
+var dineoutOptions = [
+  ["Betty's Burgers", "", "https://www.bettysburgers.com.au/menu/"],
+  ["Schnitz", "", "https://schnitz.com.au/our-menu/"],
+  ["OiSushi", "", "https://www.westfield.com.au/northlakes/store/5Pys5uw3RYMmQM842OC2wI/oi-sushi"],
+  ["Cafe63","","https://cafe63.com.au/"],
+  ["Little Red Dumpling","","https://www.littlereddumpling.com.au/menu/"]
+];
+
+var slowandsavouryOptions = [
+  ["Roast lamb", "with roast vegetables", 6],
+  ["Roast chicken", "with roast vegetables", 6],
+  ["Roast pork", "with roast vegetables", 6],
+  ["Corned Beef", "with roast vegetables", 6],
+  ["Chilli Con Carne","Women's Weekly Slow Cooker, page 25", ""],
+  ["Simple Beef & Vegetable Casserole","Women's Weekly Slow Cooker, page 30", ""],
+  ["Massaman Beef Curry","Women's Weekly Slow Cooker, page 57", ""],
+  ["Pea and Ham Soup","Women's Weekly Slow Cooker, page 162", ""],
+  ["Shredded Mexican Chicken & Beans","Women's Weekly Slow Cooker, page 180", ""],
+  ["Butter Chicken","Women's Weekly Slow Cooker, page 257", ""],
+  ["Creamy Potato Bake","Women's Weekly Slow Cooker, page 289", ""],
+  ["Pumpkin Soup","Women's Weekly Slow Cooker, page 297", ""],
+  ["Cauliflower Cheese","Women's Weekly Slow Cooker, page 310", ""],
+  ["Lamb Korma","Women's Weekly Slow Cooker, page 364", ""]
+];
